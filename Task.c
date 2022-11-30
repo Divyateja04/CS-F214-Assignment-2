e13423ed039160c7b57a8e78688770b0a0c2e91d
Divyateja Pasupuleti <divyateja2004@gmail.com>
2022-11-30 21:06:51 +0530

    Take input from file

--- Task.c
+++ Task.c
@@ -16,33 +16,30 @@
 
 int main(){
     /**
+     * @brief Create a file pointer to open the text file
+     * 
+     */
+    FILE *file = fopen("./input.txt", "r");
+
+    /**
      * @brief Take a constant to take care of number of lines of input
      * 
      */
     int noOfLines;
-    scanf("%d", &noOfLines);
+    fscanf(file, "%d\n", &noOfLines);
+    printf("Found %d lines\n\n", noOfLines);
 
     /**
-     * @brief Creating an array of strings to store input
+     * @brief Creating an array of strings to store input and the statements
      * 
      */
     char lines[noOfLines][max];
+    char statements[noOfLines][max];
 
     // Taking input
     for(int i=0; i<noOfLines; i++){
-        //To ensure we do not read the new ilne
-        fflush(stdin);
         //Getting input into the array of strings
-        fgets(lines[i], max, stdin);
-    }
-
-    /**
-     * @brief Create an array to keep track of premises and every true statement
-     * 
-     */
-    int premises[letters];
-    for(int i=0; i<letters; i++){
-        premises[i] = 0;
+        fgets(lines[i], max, file);
     }
 
     /**
@@ -51,14 +48,25 @@ int main(){
      * 
      */
     for(int i=0; i<noOfLines; i++){
+        // Replace every \n with \0
+        if(lines[i][strlen(lines[i]) - 1] == '\n') lines[i][strlen(lines[i]) - 1] = '\0';
+
+        // Add only the statement to statements
+        int statementCursorTracker = 0;
+        while(lines[i][statementCursorTracker] != '/'){
+            statements[i][statementCursorTracker] = lines[i][statementCursorTracker];
+            statementCursorTracker++;
+        }
+
         // Case for if we found a premise
-        if(lines[i][strlen(lines[i])- 2] == 'P'){
-            printf("#%d: Found a Premise %s", i+1, lines[i]);
+        if(lines[i][strlen(lines[i])-1] == 'P'){
+            printf("#%d: Found a Premise %s --> VALID\n", i+1, lines[i]);
+
         }
 
         // Otherwise we check the number of / it has
         else{
-            printf("#%d: Found a Statement %s", i+1, lines[i]);
+            printf("#%d: Found a Statement %s --> ", i+1, lines[i]);
 
             int j = 0;
             int slashes = 0;
@@ -68,7 +76,11 @@ int main(){
                 //Once it reaches the first slash
                 if(lines[i][j] == '/' && slashes == 0){
                     j++;
-                    // If the next character is ^
+
+                    /**
+                     * @brief Construct a new if object for ^
+                     * 
+                     */
                     if(lines[i][j] == '^'){
                         j++;
                         // We check if it's Introduction or Elimination
@@ -87,8 +99,8 @@ int main(){
                                 lineNo2 = lineNo2*10 + ((int) (lines[i][j])) - 48;
                                 j++;
                             }
-
-                            printf("And Introduction - Lines %d %d", lineNo1, lineNo2);
+                            printf("And Introduction - Lines %d %d ", lineNo1, lineNo2);
+                            printf("%s^%s", statements[lineNo1-1], statements[lineNo2-1]);
                         }
                         else if(lines[i][j] == 'e'){
                             j++;
@@ -98,16 +110,19 @@ int main(){
                             // Go forward to find the line number
                             j += 2;
 
-                            int lineNo1 = 0;
+                            int lineNo = 0;
                             while(((int) lines[i][j]) > 48 && ((int) lines[i][j]) < 58){
-                                lineNo1 = lineNo1*10 + ((int) (lines[i][j])) - 48;
+                                lineNo = lineNo*10 + ((int) (lines[i][j])) - 48;
                                 j++;
                             }
 
-                            printf("And Elimination - Mode %d - Line %d", eliminationMode, lineNo1);
+                            printf("And Elimination - Mode %d - Line %d", eliminationMode, lineNo);
                         }
                     }
-                    // If the next character is v
+                    /**
+                     * @brief Construct a new if object for v
+                     * 
+                     */
                     else if(lines[i][j] == 'v'){
                         j++;
                         if(lines[i][j] == 'i'){
@@ -118,16 +133,19 @@ int main(){
                             // Go forward to find the line number
                             j += 2;
 
-                            int lineNo1 = 0;
+                            int lineNo = 0;
                             while(((int) lines[i][j]) > 48 && ((int) lines[i][j]) < 58){
-                                lineNo1 = lineNo1*10 + ((int) (lines[i][j])) - 48;
+                                lineNo = lineNo*10 + ((int) (lines[i][j])) - 48;
                                 j++;
                             }
 
-                            printf("Or Elimination - Mode %d - Line %d", eliminationMode, lineNo1);
+                            printf("Or Elimination - Mode %d - Line %d", eliminationMode, lineNo);
                         }
                     }
-                    // If the next character is >
+                    /**
+                     * @brief Construct a new if object for >
+                     * 
+                     */
                     else if(lines[i][j] == '>' || lines[i][j] == 'M'){
                         printf("MT or > Elimination");
                     }
